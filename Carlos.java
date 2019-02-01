4085165165a19e7c00128058ab9994525cadc32d
Josh <jmcclint@unb.ca>
2019-02-01 15:43:35 -0400

    Made some changes

--- Carlos.java
+++ Carlos.java
@@ -1,5 +1,6 @@
 public class Carlos {
     public static void main(String[] args) {
-        System.out.print("Carlos");
-    }
+        System.out.println("Carlos");
+	System.out.println("I am invading!");   
+ }
 }
